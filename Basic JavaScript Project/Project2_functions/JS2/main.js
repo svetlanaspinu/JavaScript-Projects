ee15b2f2b9c2880a006694ea0d4b43c5752c4ea5
svetlanaspinu <s.spinu@yahoo.com>
2023-08-02 09:21:23 +0100

    Prpject2_Functions Assignment

---
+++ Basic JavaScript Project/Project2_functions/JS2/main.js
@@ -0,0 +1,19 @@
+
+// using the .getElementById method
+function My_first_function() {
+    var three = "The page is loading"
+    document.getElementById("Button_text").innerHTML = three;
+
+}
+
+// using the += operators
+function myFunction() {
+    var sentence ="I am learning";
+    sentence += " a lot from this book.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+
+}
+
+
+
+
