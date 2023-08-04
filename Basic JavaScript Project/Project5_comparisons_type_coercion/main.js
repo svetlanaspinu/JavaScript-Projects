36ff7bf5ffd63c9a822d65ec0988678638422234
svetlanaspinu <s.spinu@yahoo.com>
2023-08-04 22:18:29 +0100

    Update main.js

--- Basic JavaScript Project/Project5_comparisons_type_coercion/main.js
+++ Basic JavaScript Project/Project5_comparisons_type_coercion/main.js
@@ -2,3 +2,33 @@ var a = 29
 document.write(a);
 
 document.write("200" - 100);
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(20 > 100);
+
+document.write(3 < 5);
+
+console.log(7 + 7);
+
+console.log(22 > 44);
+
+document.write(34 == 34);
+document.write(50 == 70);
+
+a = "apple";
+a = "appple";
+document.write(a === a);
+
+c = "bread";
+d = 23;
+document.write(c === d);
+
+e = 'grapes';
+e = 'books';
+document.write(e === e);
+
+
+
