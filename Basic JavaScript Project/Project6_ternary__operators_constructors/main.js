30df858db8ddeb243bc480b60092c5bfe20e46f0
svetlanaspinu <s.spinu@yahoo.com>
2023-08-08 17:31:47 +0100

    Project_Final6

--- Basic JavaScript Project/Project6_ternary__operators_constructors/main.js
+++ Basic JavaScript Project/Project6_ternary__operators_constructors/main.js
@@ -7,10 +7,40 @@ function Ride_Function() {
 
 document.write("br")
 
-// Ternary Operatoe Challenge
+// Ternary Operation Challenge
 function Vote_Function() {
     var Age1, Age2;
     Age1 = document.getElementById("Age").value;
     Age2 = (Age1 > 18) ? "You can " : "You are not old enough to";
     document.getElementById("Vote").innerHTML = Age2 + " vote!";
 }
+
+function Vote_Function() {
+    var Age1, Age2;
+    Age1 = document.getElementById("Age").value;
+    Age2 = (Age1 > 18) ? "You can " : "You are not old enough to";
+    document.getElementById("New_and_This").innerHTML = Age2 + " vote!";
+}
+
+// Nested Function
+function Nested_Function() {
+    document.getElementById("Counting").innerHTML = Stop() ;
+    function Stop(){
+        var start = 4;
+        function Walk(){Stop += 6;}
+        Walk();
+        return start
+    }
+}
+
+// Constructor  and using this and new keywords.
+function Names(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+  const Tom = new Names("Tom", "Holland", 23, "blue");
+  const Bella = new Names("Bella", "Flower", 29, "green");
+
+  document.getElementById("Const").innerHTML ="Tom is" + Tom.age + ", and Bella is" + Bella.age +".";
