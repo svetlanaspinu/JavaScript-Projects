e980a905e2de40fd6a505429ef92d6c38db3f120
svetlanaspinu <s.spinu@yahoo.com>
2023-08-09 09:43:56 +0100

    If/Else_Statement

---
+++ Basic JavaScript Project/Project7_scope_time_function/main.js
@@ -0,0 +1,67 @@
+// Global Variable
+var a = "apples";
+function Fruit(){
+    document.write("Anna's favorite fruits are " + a + ".");
+}
+
+function Fruit_2() {
+    document.write("Emmy's favorite fruits are " + a + " too.");
+}
+
+Fruit();
+document.write("<br>");
+Fruit_2();
+
+document.write("<br>");
+// Local Variable 
+function Local() {
+    var b = "kiwi";
+    document.write("Ella loves " + b + ".");
+}
+
+function Local_2() {
+    document.write("John loves " + b + ".");
+}
+Local();
+Local_2();
+
+// using console.long - access using ctrl+shift+J
+function Local() {
+    var b = "kiwi";
+    console.log("Ella loves " + b + ".");
+}
+
+function Local_2() {
+    console.log("John loves " + b + ".");
+}
+Local();
+Local_2();
+document.write("<br>");
+
+// Method Assignement
+function Time() {
+    const a = new Date();
+    document.getElementById("time").innerHTML = "Today is " + a;
+}
+
+// using if statement
+function Statement() {
+    x = 2;
+    y = 4;
+    if (x < y) {
+        alert("Hello");
+    }
+}
+
+// using else statement
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 10) {
+        Icecream = "You can not have free icecream!";
+    }
+    else {
+        Icecream = "Free icecream for you!";
+    }
+    document.getElementById("Your_Age").innerHTML = Icecream;
+}
+
