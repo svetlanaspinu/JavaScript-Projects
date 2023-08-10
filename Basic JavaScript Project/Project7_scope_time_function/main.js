8019273a186d9034980b6d6e4dd406c58826c83c
svetlanaspinu <s.spinu@yahoo.com>
2023-08-10 21:46:04 +0100

    Final

--- Basic JavaScript Project/Project7_scope_time_function/main.js
+++ Basic JavaScript Project/Project7_scope_time_function/main.js
@@ -65,3 +65,21 @@ function Age_Function() {
     document.getElementById("Your_Age").innerHTML = Icecream;
 }
 
+// Else If Statement
+
+function Time_Function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 ==Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
+
+
