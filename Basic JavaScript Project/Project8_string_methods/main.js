fa4ec680c890b8795a5b6509d1b3c3c354142f96
svetlanaspinu <s.spinu@yahoo.com>
2023-08-11 22:06:49 +0100

    String methods

--- Basic JavaScript Project/Project8_string_methods/main.js
+++ Basic JavaScript Project/Project8_string_methods/main.js
@@ -8,3 +8,24 @@ function String_Method() {
     var all_one = a.concat(b, c, d);
     document.getElementById("Concatenate").innerHTML = all_one;
 }
+
+// using slice() method
+function Slice_Method() {
+    var a = "Tomorrow is may day off."
+    var b = a.slice(19);
+    document.getElementById("Slice").innerHTML = b;
+}
+
+
+//using toUpperCase() method
+function Upper_Case() {
+    let string = document.getElementById("upper").innerHTML;
+    document.getElementById("upper").innerHTML = string.toUpperCase();
+}
+
+// using serach() method
+let a = "Please locate where 'locate' occurs!";
+let b = a.search("locate");
+document.getElementById("search").innerHTML = b;
+
+
