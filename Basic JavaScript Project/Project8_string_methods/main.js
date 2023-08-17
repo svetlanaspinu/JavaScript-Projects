c3f1b71f5740eed2d651ea7313906dc32334dc0a
svetlanaspinu <s.spinu@yahoo.com>
2023-08-17 14:12:22 +0100

    Project_8

--- Basic JavaScript Project/Project8_string_methods/main.js
+++ Basic JavaScript Project/Project8_string_methods/main.js
@@ -28,4 +28,27 @@ let a = "Please locate where 'locate' occurs!";
 let b = a.search("locate");
 document.getElementById("search").innerHTML = b;
 
+// using toString() method
+function String_method() {
+    var S = 999;
+    document.getElementById("string_numbers").innerHTML = S.toString();
+}
+
+// using toPrecision() method
+function precision_method() {
+    var P = 98.89;
+    document.getElementById("precision").innerHTML = P.toPrecision(5);
+}
+
+// using toFixed() method
+function Fixed_method() {
+    var F = 5.65;
+    document.getElementById("fixed").innerHTML = F.toFixed(10);
+}
+
+// using .Valueof() method
+function value_method() {
+    var V = 200;
+    document.getElementById("value").innerHTML = V.valueOf();
+}
 
