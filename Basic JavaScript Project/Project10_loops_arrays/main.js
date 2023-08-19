6c4b0fc11db0606649aee6f967afe30155b7aa0b
svetlanaspinu <s.spinu@yahoo.com>
2023-08-19 22:01:48 +0100

    Working with Array

--- Basic JavaScript Project/Project10_loops_arrays/main.js
+++ Basic JavaScript Project/Project10_loops_arrays/main.js
@@ -7,3 +7,26 @@ function Call_Loop() {
     }
     document.getElementById("looping").innerHTML = colors;
 }
+
+// using For Loop
+var Countries = ["France", "Spain", "Greece", "Poland", "Albania", "Georgia"];
+var Content = "";
+var X;
+
+function Countries_names() {
+    for (X=0; X < Countries.length; X++ ) {
+        Content += Countries[X] + "<br>";
+    }
+    document.getElementById("Countries").innerHTML = Content;
+
+}
+
+// using Array
+function Array_Function() {
+    var Forecast = [];
+    Forecast[0] = "raining";
+    Forecast[1] = "sunny";
+    Forecast[2] = "windy";
+    Forecast[3] = "snowing";
+    document.getElementById("Array").innerHTML = "The weather is amazing. Today is " + Forecast[1] + "."
+}
