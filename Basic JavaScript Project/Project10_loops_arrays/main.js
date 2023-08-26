0aa583108b291d3bbf04a93468a2f6599b1ed08f
svetlanaspinu <s.spinu@yahoo.com>
2023-08-26 21:51:58 +0100

    Project10_Final

--- Basic JavaScript Project/Project10_loops_arrays/main.js
+++ Basic JavaScript Project/Project10_loops_arrays/main.js
@@ -32,10 +32,55 @@ function Array_Function() {
 }
 
 // using const keyword
-
 function Constant_function() {
     const Soup_ofthe_day = {soup_1:"tomato soup", soup_2:"carrot soup",soup_3:"mushroom soup"};
     Soup_ofthe_day.price = "3£";
     Soup_ofthe_day.quantity = "300ml";
     document.getElementById("Constant").innerHTML = "Soup of the day is " + Soup_ofthe_day.soup_1 + ", and price is " + Soup_ofthe_day.price + ".";
 }
+
+// using let keyword
+function let_function() {
+    let A = "apple";
+    document.getElementById("demo").innerHTML = A;
+}
+
+// using return statement
+function return_stat(a, b) {
+    a = 4
+    b = 3
+    var r;
+    r = a * b;
+    return(r);
+    
+}
+
+// using objects
+let cat = {
+    type:"Mammal",
+    diet:"Carnivore",
+    size:"71 cm",
+    acivity: "Running",
+    description: function() {
+        return "The cat is a " + this.size + this.type + this.diet + ", and is " + this.acivity + " all day!";   
+    }
+};
+document.getElementById("Cat_Object").innerHTML = cat.description();
+
+// using break statement in a loop
+let text = "";
+for (let i = 0; i < 10; i++) {
+  if (i === 3) { break; }
+  text += "The number is " + i + "<br>";
+}
+document.getElementById("break").innerHTML = text;
+
+// a loop with continue Statement
+let statement = "";
+for (let i = 0; i < 10; i++) {
+  if (i === 3) { continue; }
+  text += "The number is " + i + "<br>";
+}
+document.getElementById("continue").innerHTML = statement;
+
+
